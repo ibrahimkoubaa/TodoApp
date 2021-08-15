3bfbcd5babf77a110bacfbe5b8e884c612c1cf47
Ibrahim Koubaa <koubaa.ibra@gmail.com>
2021-08-15 21:23:21 +0100

    final push

--- js/app.js
+++ js/app.js
@@ -1,167 +1,99 @@
-// call search bar
-let searchBar = document.querySelector('.search');
-// call input of adding note
-let noteInput = document.querySelector('.add-cart');
-// call button of submitting a note
-let btnSubmit = document.getElementById('cta-submit');
-// call ul list 
-let list = document.querySelector('#carts');
-let inputRadio = document.querySelector('#check');
+let todoApp = [];
+let newTodo = document.getElementById('newTodo');
+let submitTodo = document.getElementById('submitTodo');
+let searchTodo = document.getElementById('searchTodo');
+let list = document.getElementById('carts');
+getLocalStorage();
 
-// adding an updating date
-let dateUpdated = document.getElementById("date");
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var today = new Date();
-var dd = String(today.getDate());
-var mm = String(months[today.getMonth()]);
-var yyyy = today.getFullYear();
-today = dd + 'th ' + mm + ', ' + yyyy;
-dateUpdated.innerHTML = today;
-
-function addContent() {
-    let newItem = document.createElement('li');
-    newItem.className = 'cart';
-    newItem.innerHTML = '<input id="check" type="radio">';
-    let textNote = document.createElement('span');
-    textNote.className = 'textnote';
-    textNote.id = Date.now();
-    newItem.append(textNote);
-    list.style.cssText += 'background-color:#FFFFFF;box-shadow:0px 4px 14px rgba(0, 0, 0, 0.14)';
-    list.prepend(newItem);
-    btnSubmit.disabled = true;
-    btnSubmit.style.cssText = 'none';
+function rendreing(todoApp) {
+    list.innerHTML = "";
+    for (let i = 0; i < todoApp.length; i++) {
+        let newItem = document.createElement('li');
+        newItem.className = 'cart';
+        newItem.id = 'itemCart';
+        newItem.innerHTML = '<input id="check" type="radio">';
+        let textNote = document.createElement('span');
+        textNote.className = 'textnote';
+        textNote.id = Date.now();
+        newItem.append(textNote);
+        list.append(newItem);
+        list.style.cssText += 'background-color:#FFFFFF;box-shadow:0px 4px 14px rgba(0, 0, 0, 0.14)';
+        list.prepend(newItem);
+        let index = todoApp[i].value;
+        textNote.textContent = index;
+    };
 };
 
-// function for adding a new item by enter key clicking
-noteInput.onkeyup = function(e) {
-    btnSubmit.disabled = false;
-    btnSubmit.style.cssText = 'cursor: pointer;color: #7556CD;background: linear-gradient( 180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0.276) 100%);'
-    if (e.keyCode === 13) {
-
-        if (noteInput.value.trim() !== "") {
-            addContent()
-            let span = document.querySelector('.textnote');
-            span.textContent = e.target.value;
-            setToLocalS(e.target.value);
-        }
-        btnSubmit.disabled = true;
-        btnSubmit.style.cssText = 'none'
-        e.target.value = "";
-    }
-};
-// function to display submitting button
-noteInput.onclick = function() {
-    if (noteInput.value !== "") {
-        btnSubmit.disabled = false;
-        btnSubmit.style.cssText = 'cursor: pointer;color: #7556CD;background: linear-gradient( 180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0.276) 100%);'
+newTodo.onkeyup = function() {
+    if (newTodo.value.trim() !== "") {
+        submitTodo.disabled = false;
+        submitTodo.style.cssText = 'cursor: pointer;color: #7556CD;background: linear-gradient( 180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0.276) 100%);'
     }
 };
 
-// function for adding a new item by mousse clicking
-btnSubmit.addEventListener('click', function() {
-    if (noteInput.value.trim() !== "") {
-        addContent()
-        let span = document.querySelector('.textnote');
-        span.textContent = noteInput.value;
-        setToLocalS(noteInput.value);
-    }
-    noteInput.value = "";
-});
-
-
-
-
-
-// function searching for a note
-searchBar.addEventListener('keyup', function(e) {
-    let contentSearch = e.target.value.toLowerCase();
-    saveSearch(contentSearch)
-    let itemsInList = list.querySelectorAll('li');
-    // change list of dom to an array to  apply forEach methode 
-    Array.from(itemsInList).forEach(function(item) {
-        let textNote = item.textContent.toLowerCase();
-        if (textNote.indexOf(contentSearch) == -1) {
-            item.style.display = 'none';
-        } else {
-            item.style.display = 'flex';
-        }
+searchTodo.addEventListener('keyup', function() {
+    let searchTodoLowerC = searchTodo.value.toLowerCase();
+    let arrFilitred = todoApp.filter((founded) => {
+        foundedValue = founded.value;
+        return foundedValue.includes(searchTodoLowerC);
     });
+    console.log(arrFilitred)
+    rendreing(arrFilitred);
+});
+
+submitTodo.addEventListener('click', () => {
+    if (newTodo.value.trim() !== "") {
+        let newAdd = { value: newTodo.value, id: Date.now() };
+        todoApp.push(newAdd);
+
+        rendreing(todoApp);
+        storeLocalStorage(newAdd)
+    };
+    newTodo.value = "";
 });
 
 
 
-
-// save in local storage
-let setToLocalS = (newElem) => {
-    if (localStorage.getItem('Local') === null) {
-        var arrayInLocal = [];
+function storeLocalStorage(added) {
+    if (localStorage.getItem('boxStorage') === null) {
+        todoApp = []
     } else {
-        arrayInLocal = JSON.parse(localStorage.getItem('Local'));
+        todoApp = JSON.parse(localStorage.getItem('boxStorage'))
     }
-    arrayInLocal.push(newElem);
-    localStorage.setItem('Local', JSON.stringify(arrayInLocal));
+    todoApp.push(added);
+    localStorage.setItem('boxStorage', JSON.stringify(todoApp))
 };
 
-function saveSearch(value) {
-    localStorage.setItem('search', value)
-};
+function getLocalStorage() {
+    if (localStorage.getItem('boxStorage') !== null) {
+        todoApp = JSON.parse(localStorage.getItem('boxStorage'))
 
-
-
-//get from local storage
-let getFromLocal = () => {
-    if (localStorage.getItem('Local') === null) {
-        arrayInLocal = [];
-    } else {
-        arrayInLocal = JSON.parse(localStorage.getItem('Local'));
     }
-    arrayInLocal.forEach((elem) => {
-        addContent();
-        let span = document.querySelector('.textnote');
-        span.textContent = elem;
-    })
+    rendreing(todoApp);
+
 };
-
-
-
-
-//remove item than remove it from a local storage
 
 list.addEventListener('click', function(e) {
+
     if (e.target.id === 'check') {
-        let message = confirm("Do you want to remove it !!");
-        if (message == true) {
-            e.target.parentElement.remove();
-            arrayInLocal = JSON.parse(localStorage.getItem('Local'));
-            let index = arrayInLocal.indexOf(e.target.parentElement);
-            arrayInLocal.splice(index, 1);
-            localStorage.setItem('Local', JSON.stringify(arrayInLocal));
-        }
-        e.target.checked = false;
-    }
-});
+        todoApp = JSON.parse(localStorage.getItem('boxStorage'));
+        todoApp.map((i) => {
 
+            if (i.value === e.target.nextSibling.innerHTML) {
+                console.log(e.target.nextSibling.id)
+                let message = confirm("Do you want to remove it !!");
+                if (message) {
 
+                    let koko = todoApp.splice(todoApp.indexOf(i.id), 1);
 
+                    e.target.parentNode.remove();
 
-document.addEventListener('DOMContentLoaded', getFromLocal);
-
-
-/*list.addEventListener('click', function(e) {
-    if (e.target.className === 'textnote') {
-        let message = confirm("Do you want to remove it !!");
-        if (message == true) {
-            let removedElem = e.target;
-
-            arrayInLocal = JSON.parse(localStorage.getItem('Local'));
-            for (let i = 0; i <= arrayInLocal.length; i++) {
-                if (removedElem.innerHTML === arrayInLocal[i]) {
-                    let index = arrayInLocal.indexOf(arrayInLocal[i]);
-                    arrayInLocal.splice(index, 1);
-                    localStorage.setItem('Local', JSON.stringify(arrayInLocal));
-                    removedElem.parentNode.remove()
                 }
+                e.target.checked = false;
             }
-        }
+        })
+
+
     }
-});*/
+    localStorage.setItem('boxStorage', JSON.stringify(todoApp))
+});
